7f0275b855ca7bcb2262f04ee35e9007ac8d7b3a
radin parsaei <radinparsaei@gmail.com>
2021-07-12 23:06:44 +0430

    Update example-plugin.js

--- example-plugin.js
+++ example-plugin.js
@@ -1,1 +1,27 @@
 console.log('hey! plugin js code executed')
+let yml = `
+---
+  Test:
+    color: ff0000
+    icon: text.svg
+    blocks:
+      - test:
+          shadows:
+            - 1
+          args:
+            a: value
+            b: statement
+          function: test
+          code: >
+            func test(a, b) {
+              print(a)
+              b!()
+            }
+          messages:
+            - test
+            -
+          hasReturn: false
+          tooltip: test
+`
+createBlocksFromYAML(yml)
+refreshBlockly()
